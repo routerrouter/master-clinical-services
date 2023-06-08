5158c8e574dd7f902f74907416e6c3986a78faf3
Rufino Domingos <32271198+routerrouter@users.noreply.github.com>
2023-06-08 17:06:39 +0100

    Habilitando o EurekaClient

--- storage/src/main/java/master/ao/storage/StorageApplication.java
+++ storage/src/main/java/master/ao/storage/StorageApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableJpaAuditing
 public class StorageApplication {
 
